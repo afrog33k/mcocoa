bf0b835e43d7a792c3a413de976cbf9277c8d412
jesjones@mindspring.com <jesjones@mindspring.com@8bd48ae8-a223-11dd-9c66-c9f8973bfffa>
2008-10-25 00:55:06 +0000

    Removing generated files.

--- trunk/source/foundation/generated/Foundation.cs
+++
@@ -1,8 +0,0 @@
-// machine generated on 10/24/2008 3:54:03 PM using /System/Library/Frameworks/Foundation.framework/Versions/Current/Headers/Foundation.h
-
-using MObjc;
-using System;
-
-namespace MCocoa
-{
-}
