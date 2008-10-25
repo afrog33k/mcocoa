bb74780dfe4f7becca8268ad2622f53103c417ce
jesjones <jesjones@mindspring.com>
2008-10-25 00:54:16 +0000

    Removing generated files.

--- source/appkit/generated/NSSpellProtocol.cs
+++
@@ -1,8 +0,0 @@
-// machine generated on 10/24/2008 3:54:07 PM using /System/Library/Frameworks/AppKit.framework/Versions/Current/Headers/NSSpellProtocol.h
-
-using MObjc;
-using System;
-
-namespace MCocoa
-{
-}
