fe2a63aab62197419172360311859f9684e8caa5
jesjones <jesjones@mindspring.com>
2009-04-03 05:53:41 +0000

    Whitespace.

--- source/appkit/Functions.cs
+++ source/appkit/Functions.cs
@@ -33,8 +33,8 @@ namespace MCocoa
 		{
 			NativeMethods.NSBeep();
 		}
-
-		#region Alert Sheets --------------------------------------------------
+		
+		#region Panels
 		public static void NSBeginAlertSheet(NSString title, NSString defaultButton, NSString alternateButton, NSString otherButton, NSWindow docWindow, NSObject modalDelegate, string didEndSelector, string didDismissSelector, IntPtr contextInfo, NSString message)
 		{
 			Selector endSelector = didEndSelector != null ? new Selector(didEndSelector) : null;
@@ -42,7 +42,7 @@ namespace MCocoa
 			
 			NativeMethods.NSBeginAlertSheet(title, defaultButton, alternateButton, otherButton, docWindow, modalDelegate, endSelector, dismissSelector, contextInfo, message);
 		}
-
+		
 		public static void NSBeginInformationalAlertSheet(NSString title, NSString defaultButton, NSString alternateButton, NSString otherButton, NSWindow docWindow, NSObject modalDelegate, string didEndSelector, string didDismissSelector, IntPtr contextInfo, NSString message)
 		{
 			Selector endSelector = didEndSelector != null ? new Selector(didEndSelector) : null;
@@ -50,7 +50,7 @@ namespace MCocoa
 			
 			NativeMethods.NSBeginInformationalAlertSheet(title, defaultButton, alternateButton, otherButton, docWindow, modalDelegate, endSelector, dismissSelector, contextInfo, message);
 		}
-
+		
 		public static void NSBeginCriticalAlertSheet(NSString title, NSString defaultButton, NSString alternateButton, NSString otherButton, NSWindow docWindow, NSObject modalDelegate, string didEndSelector, string didDismissSelector, IntPtr contextInfo, NSString message)
 		{
 			Selector endSelector = didEndSelector != null ? new Selector(didEndSelector) : null;
@@ -58,141 +58,137 @@ namespace MCocoa
 			
 			NativeMethods.NSBeginCriticalAlertSheet(title, defaultButton, alternateButton, otherButton, docWindow, modalDelegate, endSelector, dismissSelector, contextInfo, message);
 		}
-		#endregion
 
-		#region Alert Panels --------------------------------------------------
 		public static int NSRunAlertPanel(NSString title, NSString message)
 		{
 			return (int) NativeMethods.NSRunAlertPanel(title, message, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
 		}
-
+		
 		public static int NSRunAlertPanel(NSString title, NSString message, NSString defaultButton, NSString alternateButton, NSString otherButton)
 		{
 			return (int) NativeMethods.NSRunAlertPanel(title, message, defaultButton, alternateButton, otherButton);
 		}
-
+		
 		public static int NSRunInformationalAlertPanel(NSString title, NSString message)
 		{
 			return (int) NativeMethods.NSRunInformationalAlertPanel(title, message, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
 		}
-
+		
 		public static int NSRunInformationalAlertPanel(NSString title, NSString message, NSString defaultButton, NSString alternateButton, NSString otherButton)
 		{
 			return (int) NativeMethods.NSRunInformationalAlertPanel(title, message, defaultButton, alternateButton, otherButton);
 		}
-
+		
 		public static int NSRunCriticalAlertPanel(NSString title, NSString message)
 		{
 			return (int) NativeMethods.NSRunCriticalAlertPanel(title, message, IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
 		}
-
+		
 		public static int NSRunCriticalAlertPanel(NSString title, NSString message, NSString defaultButton, NSString alternateButton, NSString otherButton)
 		{
 			return (int) NativeMethods.NSRunCriticalAlertPanel(title, message, defaultButton, alternateButton, otherButton);
 		}
 		#endregion
-
-		#region Framing Rectangles --------------------------------------------
+		
+		#region Graphics
 		public static void NSFrameRect(NSRect rect)
 		{
 			NativeMethods.NSFrameRect(rect);
 		}
-
+		
 		public static void NSFrameRectWithWidth(NSRect rect, float width)
 		{
 			NativeMethods.NSFrameRectWithWidth(rect, width);
 		}
-
+		
 		public static void NSFrameRectWithWidthUsingOperation(NSRect rect, float width, int op)
 		{
 			NativeMethods.NSFrameRectWithWidthUsingOperation(rect, width, op);
 		}
-		#endregion
 
-		#region Filling Rectangles --------------------------------------------
 		public static void NSRectFill(NSRect rect)
 		{
 			NativeMethods.NSRectFill(rect);
 		}
-
+		
 		public static void NSRectFillList(NSRect[] rects)
 		{
 			NativeMethods.NSRectFillList(rects, rects.Length);
 		}
-
+		
 		public static void NSRectFillListUsingOperation(NSRect[] rects, int op)
 		{
 			NativeMethods.NSRectFillListUsingOperation(rects, rects.Length, op);
 		}
-
+		
 		public static void NSRectFillListWithColors(NSRect[] rects, NSColor[] colors)
 		{
 			Trace.Assert(rects.Length == colors.Length, "rects and colors have different lengths");
 			
 			NativeMethods.NSRectFillListWithColors(rects, colors, rects.Length);
 		}
-
+		
 		public static void NSRectFillListWithColorsUsingOperation(NSRect[] rects, NSColor[] colors, int op)
 		{
 			Trace.Assert(rects.Length == colors.Length, "rects and colors have different lengths");
 			
 			NativeMethods.NSRectFillListWithColorsUsingOperation(rects, colors, rects.Length, op);
 		}
-
+		
 		public static void NSRectFillUsingOperation(NSRect rect, int op)
 		{
 			NativeMethods.NSRectFillUsingOperation(rect, op);
 		}
 		#endregion
-
-		#region P/Invokes -----------------------------------------------------
+		
+		#region P/Invokes
 		private static partial class NativeMethods
 		{
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSBeep();
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSFrameRect(NSRect rect);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSFrameRectWithWidth(NSRect rect, float width);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSFrameRectWithWidthUsingOperation(NSRect rect, float width, int op);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFill(NSRect rect);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFillList(NSRect[] rects, int count);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFillListUsingOperation(NSRect[] rects, int count, int op);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFillListWithColors(NSRect[] rects, NSColor[] colors, int count);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFillListWithColorsUsingOperation(NSRect[] rects, NSColor[] colors, int count, int op);
-	
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSRectFillUsingOperation(NSRect rect, int op);
 			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSBeginAlertSheet(IntPtr title, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton, IntPtr docWindow, IntPtr modalDelegate, IntPtr didEndSelector, IntPtr didDismissSelector, IntPtr contextInfo, IntPtr msgFormat);
-
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSBeginInformationalAlertSheet(IntPtr title, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton, IntPtr docWindow, IntPtr modalDelegate, IntPtr didEndSelector, IntPtr didDismissSelector, IntPtr contextInfo, IntPtr msgFormat);
-
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static void NSBeginCriticalAlertSheet(IntPtr title, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton, IntPtr docWindow, IntPtr modalDelegate, IntPtr didEndSelector, IntPtr didDismissSelector, IntPtr contextInfo, IntPtr msgFormat);
-
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static int NSRunAlertPanel(IntPtr title, IntPtr msgFormat, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton);
-
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static int NSRunInformationalAlertPanel(IntPtr title, IntPtr msgFormat, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton);
-
+			
 			[DllImport("/System/Library/Frameworks/AppKit.framework/AppKit")]
 			public extern static int NSRunCriticalAlertPanel(IntPtr title, IntPtr msgFormat, IntPtr defaultButton, IntPtr alternateButton, IntPtr otherButton);
 		}
