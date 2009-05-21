105eafcf7decb82903660a4c2d5365e3ef1e1d80
jesjones <jesjones@mindspring.com>
2009-05-21 18:12:35 +0000

    Index: source/foundation/NSData.cs Index: generate/Frameworks.xml * Replaced the NSData bytes, getBytes, getBytes_length, and getBytes_range methods    with versions that use byte[] instead of IntPtr.
    Index: examples/animating-views/SimpleLayoutView.cs
    * Whitespace.

---
+++ source/foundation/NSData.cs
@@ -0,0 +1,88 @@
+// Copyright (C) 2009 Jesse Jones
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using MObjc;
+using MObjc.Helpers;
+using System;
+using System.Runtime.InteropServices;
+
+namespace MCocoa
+{
+	public partial class NSData : NSObject
+	{
+		[Pure]
+		public byte[] bytes()
+		{
+			IntPtr ptr = DoGetBytes();
+			
+			int len = (int) length();
+			byte[] buffer = new byte[len];
+			Marshal.Copy(ptr, buffer, 0, len);
+			
+			return buffer;
+		}
+		
+		[Pure]
+		public void getBytes(out byte[] buffer)
+		{
+			buffer = bytes();
+		}
+		
+		[Pure]
+		public void getBytes_length(out byte[] buffer, UInt32 length)
+		{
+			IntPtr ptr = DoGetBytes();
+			
+			int len = (int) Math.Min(this.length(), length);
+			buffer = new byte[len];
+			Marshal.Copy(ptr, buffer, 0, (int) length);
+		}
+		
+		[Pure]
+		public void getBytes_range(out byte[] buffer, NSRange range)
+		{
+			if (range.location < 0)
+				throw new ArgumentException("range.location is negative");
+			if (range.length < 0)
+				throw new ArgumentException("range.length is negative");
+			if (range.location + range.length > length())
+				throw new ArgumentException("range upper bound is too large");
+			
+			IntPtr ptr = DoGetBytes();
+			
+			buffer = new byte[range.length];
+			Marshal.Copy(ptr, buffer, range.location, range.length);
+		}
+		
+		#region Private Methods
+		[Pure]
+		private IntPtr DoGetBytes()
+		{
+			IntPtr exception_ = IntPtr.Zero;
+			IntPtr result_ = DirectCalls.Callp(this, new Selector("bytes"), ref exception_);
+			if (exception_ != IntPtr.Zero)
+				CocoaException.Raise(exception_);
+			
+			return result_;
+		}
+		#endregion
+	}
+}
