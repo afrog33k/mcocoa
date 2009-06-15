3612c04b6a9cf99d361ab4933695776a16c3d995
jesjones <jesjones@mindspring.com>
2009-06-15 20:31:39 +0000

    Index: gen_version.sh Index: get_version.sh * Replaced gen_version.sh and get_version.sh with the ones (now) installed    by mobjc.
    Index: Makefile
    * Reworked examples so that they are built just like user apps.
    
    Index: examples/animating-views/AssemblyInfo.cs
    Index: examples/animating-views/Makefile
    * AssemblyVersion.cs is now generated by the make file.
    * Uses a local bin directory instead of the mcocoa one.
    * Grab the nib files using a wild card.
    * Added update-libraries, clean, and help targets.

--- examples/animating-views/AssemblyInfo.cs
+++ examples/animating-views/AssemblyInfo.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("0.1.0.0")]        // majorVersion.minorVersion.buildNumber.revision
 [assembly: AssemblyTitle("Sample")]
 [assembly: AssemblyDescription("obj3-sharp sample program")]
 [assembly: AssemblyCopyright("Copyright (C) 2008 Jesse Jones")]
